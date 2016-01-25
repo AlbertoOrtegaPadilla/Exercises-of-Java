25bc333c7cdf401590eb391c1d65d053629dab48
AlbertoOrtegaPadilla <albertoortega576@gmail.com>
2016-01-25 10:51:17 +0100

    Actualizar

---
+++ 7 Arrays/bidimensial/Ejercicio7.java
@@ -0,0 +1,15 @@
+/**
+ * Ejercicio 7 Mejora el juego “Busca el tesoro” de tal forma que si hay una mina a una casilla de distancia,
+ * el programa avise diciendo ¡Cuidado! ¡Hay una mina cerca!
+ * 
+ * 
+ */
+
+
+public class Ejercicio7 {
+	
+	public static void main (String args[]) {
+		
+	}
+}
+
