8e8b7f6a671260bfd6bbb051063aa10cbfc178e4
AlbertoOrtegaPadilla <albertoortega576@gmail.com>
2016-01-25 18:01:57 +0100

    Actualizar

---
+++ 7 Arrays/bidimensial/Ejercicio8.java
@@ -0,0 +1,31 @@
+/**
+ * 
+ * Ejercicio 8 Escribe un programa que, dada una posición en un tablero de ajedrez, nos diga a qué casillas
+ * podría saltar un alfil que se encuentra en esa posición. Como se indica en la figura, el alfil
+ * se mueve siempre en diagonal. El tablero cuenta con 64 casillas. Las columnas se indican
+ * con las letras de la “a” a la “h” y las filas se indican del 1 al 8.
+ * 
+ * @author Alberto Ortega Padilla
+ * 
+ */
+
+
+public class Ejercicio8 {
+	
+	public static void main (String args[]) {
+    
+    int[][] tablero = new int[8][8];
+    int fila;
+    String columna;
+    
+    
+      
+      System.out.print("Coordenada x: ");
+      x = Integer.parseInt(System.console().readLine());
+      System.out.print("Coordenada y: ");
+      y = Integer.parseInt(System.console().readLine());
+      
+		
+	}
+}
+
